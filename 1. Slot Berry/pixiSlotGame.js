0e651aab29d4721ad3adb64ef05eb6be5c829dc4
akushwaha <akushwaha@ingenuitygaming.com>
2023-08-21 12:34:36 +0530

    SYMBOL_HEIGHT

--- 1. Slot Berry/pixiSlotGame.js
+++ 1. Slot Berry/pixiSlotGame.js
@@ -16,7 +16,7 @@ app.loader
     .load(onAssetsLoaded);
 
 const REEL_WIDTH = 160;
-const SYMBOL_SIZE = 150;
+const SYMBOL_HEIGHT = 150;
 
 /* Step 4: onAssetsLoaded build the game */
 function onAssetsLoaded() {
@@ -36,7 +36,7 @@ function onAssetsLoaded() {
         rc.x = i * REEL_WIDTH;                          // Difference between reels is i * REEL_WIDTH.
         reelContainer.addChild(rc);                     // Add all reels one by one to reelContainer.
 
-        const reel = {
+        const reel = {                                  // Reel Object will have 4 properties.
             container: rc,
             symbols: [],
             position: 0,
@@ -44,12 +44,11 @@ function onAssetsLoaded() {
         };
 
         // Build the symbols
-        for (let j = 0; j < 4; j++) {
-            const symbol = new PIXI.Sprite(slotTextures[Math.floor(Math.random() * slotTextures.length)]);
-            // Scale the symbol to fit symbol area.
-            symbol.y = j * SYMBOL_SIZE;
-            symbol.scale.x = symbol.scale.y = Math.min(SYMBOL_SIZE / symbol.width, SYMBOL_SIZE / symbol.height);
-            symbol.x = Math.round((SYMBOL_SIZE - symbol.width) / 2);
+        for (let j = 0; j < 4; j++) {                   // Every reel will have 4 symbols.
+            const symbol = new PIXI.Sprite(slotTextures[Math.floor(Math.random() * slotTextures.length)]);   // Symbols will be generated randomly[0-3]
+            symbol.y = j * SYMBOL_HEIGHT;                 // Scale the symbol to fit symbol area.
+            symbol.scale.x = symbol.scale.y = Math.min(SYMBOL_HEIGHT / symbol.width, SYMBOL_HEIGHT / symbol.height);
+            symbol.x = Math.round((SYMBOL_HEIGHT - symbol.width) / 2);
             reel.symbols.push(symbol);
             rc.addChild(symbol);
         }
@@ -58,7 +57,7 @@ function onAssetsLoaded() {
     app.stage.addChild(reelContainer);
 
     // Build top & bottom covers and position reelContainer
-    const margin = (app.screen.height - SYMBOL_SIZE * 3) / 2;
+    const margin = (app.screen.height - SYMBOL_HEIGHT * 3) / 2;
     reelContainer.y = margin;
     reelContainer.x = Math.round(app.screen.width - REEL_WIDTH * 5);
     const top = new PIXI.Graphics();
@@ -66,7 +65,7 @@ function onAssetsLoaded() {
     top.drawRect(0, 0, app.screen.width, margin);
     const bottom = new PIXI.Graphics();
     bottom.beginFill(0, 1);
-    bottom.drawRect(0, SYMBOL_SIZE * 3 + margin, app.screen.width, margin);
+    bottom.drawRect(0, SYMBOL_HEIGHT * 3 + margin, app.screen.width, margin);
 
     // Add play text
     const style = new PIXI.TextStyle({
@@ -139,13 +138,13 @@ function onAssetsLoaded() {
             for (let j = 0; j < r.symbols.length; j++) {
                 const s = r.symbols[j];
                 const prevy = s.y;
-                s.y = ((r.position + j) % r.symbols.length) * SYMBOL_SIZE - SYMBOL_SIZE;
-                if (s.y < 0 && prevy > SYMBOL_SIZE) {
+                s.y = ((r.position + j) % r.symbols.length) * SYMBOL_HEIGHT - SYMBOL_HEIGHT;
+                if (s.y < 0 && prevy > SYMBOL_HEIGHT) {
                     // Detect going over and swap a texture.
                     // This should in proper product be determined from some logical reel.
                     s.texture = slotTextures[Math.floor(Math.random() * slotTextures.length)];
-                    s.scale.x = s.scale.y = Math.min(SYMBOL_SIZE / s.texture.width, SYMBOL_SIZE / s.texture.height);
-                    s.x = Math.round((SYMBOL_SIZE - s.width) / 2);
+                    s.scale.x = s.scale.y = Math.min(SYMBOL_HEIGHT / s.texture.width, SYMBOL_HEIGHT / s.texture.height);
+                    s.x = Math.round((SYMBOL_HEIGHT - s.width) / 2);
                 }
             }
         }
